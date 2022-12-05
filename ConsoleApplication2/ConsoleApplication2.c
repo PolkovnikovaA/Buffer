7f5a337a9f4b5978cb97b822cd2434f07b65d8f3
21nastya.chernaya19@gmail.com <21nastya.chernaya19@gmail.com>
2022-12-05 17:10:51 +0300

    +

--- ConsoleApplication2/ConsoleApplication2.c
+++ ConsoleApplication2/ConsoleApplication2.c
@@ -15,12 +15,67 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine
 		TCHAR third[512];
 
 		swprintf(third, sizeof third, L"%s%s", Alert, Data);
-		if (*Data != 0)
+		switch (*Data)
+		{
+		case '1':
+			MessageBoxW(NULL,  L"Один", NULL, MB_OK | MB_ICONWARNING);
+			Data = L"Один";
+			ClipboardInputText(Data);
+			break;
+		case '2':
+			MessageBoxW(NULL, L"Два", NULL, MB_OK | MB_ICONWARNING);
+			Data = L"Два";
+			ClipboardInputText(Data);
+			break;
+		case '3':
+			MessageBoxW(NULL, L"Три", NULL, MB_OK | MB_ICONWARNING);
+			Data = L"Три";
+			ClipboardInputText(Data);
+			break;
+		case '4':
+			MessageBoxW(NULL, L"Четыре", NULL, MB_OK | MB_ICONWARNING);
+			Data = L"Четыре";
+			ClipboardInputText(Data);
+			break;
+		case '5':
+			MessageBoxW(NULL, L"Пять", NULL, MB_OK | MB_ICONWARNING);
+			Data = L"Пять";
+			ClipboardInputText(Data);
+			break;
+		case '6':
+			MessageBoxW(NULL, L"Шесть", NULL, MB_OK | MB_ICONWARNING);
+			Data = L"Шесть";
+			ClipboardInputText(Data);
+			break;
+		case '7':
+			MessageBoxW(NULL, L"Семь", NULL, MB_OK | MB_ICONWARNING);
+			Data = L"Семь";
+			ClipboardInputText(Data);
+			break;
+		case '8':
+			MessageBoxW(NULL, L"Восемь", NULL, MB_OK | MB_ICONWARNING);
+			Data = L"Восемь";
+			ClipboardInputText(Data);
+			break;
+		case '9':
+			MessageBoxW(NULL, L"Девять", NULL, MB_OK | MB_ICONWARNING);
+			Data = L"Девять";
+			ClipboardInputText(Data);
+			break;
+		case '0':
+			MessageBoxW(NULL, L"Ноль", NULL, MB_OK | MB_ICONWARNING);
+			Data = L"Ноль";
+			ClipboardInputText(Data);
+			break;
+		default:
+			break;
+		}
+		/*if (*Data != 0)
 		{
 			
-			//MessageBoxW(NULL, &third, L"Внимание!!! Нарушение!!!", MB_OK | MB_ICONWARNING);
+			MessageBoxW(NULL, &third, L"Внимание!!! Нарушение!!!", MB_OK | MB_ICONWARNING);
 			ClipboardInputText("");
-		}
+		}*/
 		Sleep(1000);
 	}
 	return 0;
